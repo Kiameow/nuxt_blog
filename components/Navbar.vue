16a79007b438ae2e9c7465b14ff23660a897d9fd
kiameow <zjzootl@qq.com>
2024-08-12 14:40:36 +0800

    feat: style the portofolio page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,7 @@ const navItems = navbarData.links;
 </script>
 
 <template>
-  <div class="w-screen fixed flex bg-white py-2 px-2 border z-50">
+  <div class="w-screen fixed flex py-2 px-2 z-50 backdrop-opacity-50 backdrop-blur-sm bg-white/100">
     <div class="w-12 flex justify-center">Logo</div>
     <div class="flex-1 flex justify-around">
       <NuxtLink v-for="item in navItems" :to="item.link">{{
--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -10,13 +10,13 @@ const projects = projectData;
 </script>
 
 <template>
-  <div class="py-20 px-20 w-full h-screen">
-    <Carousel class="relative w-full">
-      <CarouselContent id="carousel" class="w-full">
+  <div class="pt-20 pb-10 px-20 w-full">
+    <Carousel class="relative w-full"> 
+      <CarouselContent>
         <CarouselItem  
-          class="md:basis-1/2 lg:basis-1/3 h-screen"
+          class="md:basis-1/1 lg:basis-7/12 h-[30rem]"
           v-for="project in projects" :key="project.name">
-          <div class="w-full h-full flex justify-center items-center border">
+          <div class="w-full h-full flex justify-center items-center bg-neutral-900 text-white rounded-xl text-3xl">
             {{ project.name }}
           </div>
         </CarouselItem>
