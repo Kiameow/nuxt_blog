1f1d92b7fdf3a4a10150915019d76cfc838d226b
kiameow <zjzootl@qq.com>
2024-09-16 23:31:45 +0800

    fix: image could not load in dev mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   nitro: {
-    static: true,
+    prerender: {
+      crawlLinks: true,
+      routes: [
+        '/',
+      ],
+    },
   },
   site: {
     url: seoData.siteURL,
